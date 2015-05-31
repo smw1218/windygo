8822b1c37bc16d3e2c929041ba6cde6d0b627a40
Scott White <swhite@fitstar.com>
2015-05-31 09:28:11 -0700

    addin pngcrush to the finish

--- finish.sh
+++ finish.sh
@@ -4,6 +4,9 @@
 composite -dissolve 20 -geometry +150+150  Boardsports_on-top-01-300x93.png windgraph.png watermarked.png
 
 # composite the current and the watermarked graph
-montage current.png watermarked.png -tile 2x1 -geometry +0+0 windreport.png 
+montage current.png watermarked.png -tile 2x1 -geometry +0+0 windreport_big.png 
+
+# crush the file
+pngcrush -q windreport_big.png windreport.png 
 
 # Add ftp here
